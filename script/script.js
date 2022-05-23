aaa0e0c1086488180bfba23ed31e64ba52608f16
Liam <liammassey21@gmail.com>
2022-05-23 20:41:49 +1000

    Add buttons to html page
    Added buttons to html page, worked on random color generator
    on script.

--- script/script.js
+++ script/script.js
@@ -2,6 +2,9 @@
 
 const container = document.getElementById('container');
 const gridColor = document.getElementsByClassName('grid');
+const buttonRandom = document.getElementById('random-rgba');
+const buttonBlack = document.getElementById('black');
+
 
 function makeGrid (rows, cols) {
     container.style.setProperty('--rows', rows);
@@ -14,8 +17,27 @@ function makeGrid (rows, cols) {
 
 makeGrid(16, 16);
 
-for (let i = 0; i < gridColor.length; i++) {
-    gridColor[i].addEventListener('mouseover', function () {
-        gridColor[i].style.backgroundColor = 'black';
-    });
+
+function colorChangeBlack () {
+    for (let i = 0; i < gridColor.length; i++) {
+        gridColor[i].addEventListener('mouseover', function () {
+            gridColor[i].style.backgroundColor = 'black';
+        });
+    };
 }
+
+function colorChangeRandom () {
+
+    for (let i = 0; i < gridColor.length; i++) {
+        gridColor[i].addEventListener('mouseover', function () {
+            let o = Math.round;
+            let r = Math.random;
+            let s = 255;
+            let color = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+            gridColor[i].style.backgroundColor = color;
+        });
+    };
+}
+
+buttonBlack.addEventListener('click', colorChangeBlack(), capture);
+buttonRandom.addEventListener('click', colorChangeRandom(), capture);
