4ef9e2923925f52d5b7c8f06493b164b71affea0
Liam <liammassey21@gmail.com>
2022-05-25 17:08:32 +1000

    Edit code in javaScript, Add design and layout
    Edited code in Script to add a user input, formated old code
    for legibitilty. Added design to button and formated layout.

--- script/script.js
+++ script/script.js
@@ -7,17 +7,42 @@ const buttonRandom = document.getElementById('random-rgba');
 const buttonBlack = document.getElementById('black');
 const erase = document.getElementById('eraser');
 const clear = document.getElementById('clear');
+const gridButton = document.getElementById('size')
 
-function makeGrid (rows, cols) {
-    container.style.setProperty('--rows', rows);
-    container.style.setProperty('--cols', cols);
-    for (let i = 0; i < (rows * cols); i++) {
+
+makeGrid(16);
+gridSize(16,16);
+
+
+function makeGrid (size) {
+    for (let i = 0; i < (size * size); i++) {
         let grid = document.createElement('div');
         container.appendChild(grid).className = 'grid';
     };
-};
+}
 
-makeGrid(16, 16);
+
+function gridSize (rows, cols) {
+    container.style.setProperty('--rows', rows);
+    container.style.setProperty('--cols', cols);
+}
+
+function changeSize() {
+    let num = prompt('Choose a number between 0 - 100');
+    console.log(num);
+    gridSize(num, num);
+    makeGrid(num);
+    clearGrid();
+
+}
+
+function clearGrid {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    };
+}
+
+gridButton.addEventListener('click', changeSize);
 
 buttonBlack.addEventListener('click', function () {
     for (let i = 0; i < gridColor.length; i++) {
@@ -55,16 +80,16 @@ clear.addEventListener('click', function () {
 })
 
 
-    for (let i = 0; i < randomColorbutton.length; i++) {
-            randomColorbutton[i].addEventListener('mouseover', function() {
-                let o = Math.round;
-                let r = Math.random;
-                let s = 255;
-                let color = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
-                randomColorbutton[i].style.backgroundColor = color;
-            });
-            randomColorbutton[i].addEventListener('mouseout', function () {
-                randomColorbutton[i].style.backgroundColor = 'white';
-            })
-    }
+for (let i = 0; i < randomColorbutton.length; i++) {
+        randomColorbutton[i].addEventListener('mouseover', function() {
+            let o = Math.round;
+            let r = Math.random;
+            let s = 255;
+            let color = 'rgba(' + o(r()*s) + ',' + o(r()*s) + ',' + o(r()*s) + ',' + r().toFixed(1) + ')';
+            randomColorbutton[i].style.backgroundColor = color;
+        });
+        randomColorbutton[i].addEventListener('mouseout', function () {
+            randomColorbutton[i].style.backgroundColor = 'white';
+        })
+}
  
